fa209bd315491631434460b43dfb2edac20dfb6d
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-05-29 22:29:55 +0200

    Insta Icon Add + Facebook id Delete

--- Body/Footer/footer.php
+++ Body/Footer/footer.php
@@ -2,10 +2,10 @@
 <footer>
     &copy; Copyright 2020 PAWS
     <div>
-        <a href="https://www.facebook.com/"><img src="../../../Images/fbicon.png"></a>
-        <a href="https://twitter.com/explore"><img src="../../../Images/twittericon.png"></a>
-        <a href="https://instagram.com/"><img src="../../../Images/instaicon.png"></a>
-        <a href="https://www.tiktok.com/en/"><img src="../../../Images/tiktokicon.png"></a>
-        <a href="https://www.linkedin.com/"><img src="../../../Images/linkedinicon.png"></a>
+        <a href="https://www.facebook.com/"><img src="../../../Images/fbicon.png" alt="fbicon"></a>
+        <a href="https://www.twitter.com/explore"><img src="../../../Images/twittericon.png" alt="twtricon""></a>
+        <a href="https://www.instagram.com/"><img src="../../../Images/instaicon.png" alt="instaicon"></a>
+        <a href="https://www.tiktok.com/en/"><img src="../../../Images/tiktokicon.png" alt="tiktokicon"></a>
+        <a href="https://www.linkedin.com/"><img src="../../../Images/linkedinicon.png" alt="lninicon"></a>
     </div>
 </footer>
