374b72f0d6b601a519c3079b83f1bfaf59662d87
TheRealDemmy <Dendimir101@yahoo.com>
2020-05-29 20:14:38 +0200

    Stuff

--- Body/Footer/footer.php
+++ Body/Footer/footer.php
@@ -2,8 +2,9 @@
 <footer>
     &copy; Copyright 2020 PAWS
     <div>
-        <img src="../../Images/fbicon.png">
-        <img src="../../Images/twittericon.png">
-        <img src="../../Images/tiktokicon.png">
+        <a href="https://www.facebook.com/"><img src="../../Images/fbicon.png"></a>
+        <a href="https://twitter.com/explore"><img src="../../Images/twittericon.png"></a>
+        <a href="https://www.tiktok.com/en/"><img src="../../Images/tiktokicon.png"></a>
+        <a href="https://www.linkedin.com/"><img src="../../Images/linkedinicon.png"></a>
     </div>
 </footer>
---
+++ Body/Websites/HomePage/HomePage.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="HomePage.css">
+    <link rel="stylesheet" type="text/css" href="../../../Header/header.css">
+    <link rel="stylesheet" type="text/css" href="../../../Footer/footer.css">
+    <link rel="stylesheet" type="text/css" href="../../../BodyLayout/BodyLayout.css">
+    <?php
+    $pageTitle = 'Home Page';
+    include('../../Header/header.php'); ?>
+</head>
+<body>
+    <main>
+        <div class="imgcycle1" style="background-color: #EFE4B0">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle1.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle2.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle3.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle4.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle5.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle6.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle7.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle8.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle9.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../../../Images/cycle10.png" alt="image">
+        </div>
+        <script type="text/javascript">
+            var slideIndex = 0;
+            carousel();
+
+            function carousel() {
+                var i;
+                var x = document.getElementsByClassName("elementToFadeInAndOut");
+                for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex > x.length) {slideIndex = 1}
+                x[slideIndex-1].style.display = "block";
+                setTimeout(carousel, 5000);
+            }
+        </script>
+
+        <img id="bone" src="../../../Images/Bone1Fix4.png" alt="Quote">
+
+        <aside>
+
+        </aside>
+    </main>
+<?php
+include('../../Footer/footer.php'); ?>
+</body>
+</html>
