6697fd14eed50c46aa8fc31c6fdc2781a6951461
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-05-29 16:39:46 +0200

    commit 1

---
+++ Body/Header/header.php
@@ -0,0 +1,25 @@
+<title><?php echo $pageTitle; ?></title>
+<link rel="stylesheet" type="text/css" href="header.css">
+<header>
+    <div class="yes">
+        <img id="logo" src="../../Images/logo-test2.png" alt="logo" style="height: 200px;">
+        <br>
+        <nav>
+            <div class="dropdown">
+                <a id="dropbtn" href="#">Animals</a>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <br>
+                    <a href="#">Link 2</a>
+                    <br>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <a href="#">Adopt</a>
+            <a href="#">About Us</a>
+            <a href="#">Contact</a>
+            <a href="#">Sign-up</a>
+        </nav>
+    </div>
+</header>
+<div style="clear: both"></div>
---
+++ Body/HomePage/HomePage.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+    <link rel="stylesheet" type="text/css" href="HomePage.css">
+    <link rel="stylesheet" type="text/css" href="../Header/header.css">
+    <?php
+    $pageTitle = 'Home Page';
+    include('../Header/header.php'); ?>
+</head>
+<body>
+    <main>
+        <div class="imgcycle1" style="background-color: #EFE4B0">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle1.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle2.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle3.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle4.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle5.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle6.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle7.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle8.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle9.png" alt="image">
+            <img class="elementToFadeInAndOut" src="../../Images/cycle10.png" alt="image">
+        </div>
+        <script type="text/javascript">
+            var slideIndex = 0;
+            carousel();
+
+            function carousel() {
+                var i;
+                var x = document.getElementsByClassName("elementToFadeInAndOut");
+                for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex > x.length) {slideIndex = 1}
+                x[slideIndex-1].style.display = "block";
+                setTimeout(carousel, 5000);
+            }
+        </script>
+
+        <img id="bone" src="../../Images/Bone1Fix4.png" alt="Quote">
+
+        <aside>
+
+        </aside>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
