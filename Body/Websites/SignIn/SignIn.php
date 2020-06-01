616acb2a6cfbaa7edb308f5528297957c5590515
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-01 22:46:59 +0200

    Refining Sign-In and Sign-Up

--- Body/Websites/SignIn/SignIn.php
+++ Body/Websites/SignIn/SignIn.php
@@ -24,15 +24,16 @@
 	        <section id="content">
 		        <form method="POST" action="SignIn.php">
 			        <h1>Sign-In</h1>
+                    <div class="Input">
+                        <p>Username:</p>
+                        <input class="Input2" type="text" placeholder="Username" id="username" name="username" />
+                    </div>
+                    <div class="Input">
+                        <p>Password:</p>
+                        <input class="Input2" type="password" placeholder="Password" name="password" id="password" />
+                    </div>
 			        <div>
-				        <input type="text" placeholder="Username" id="username" name="username" />
-			        </div>
-			        <div>
-				        <input type="password" placeholder="Password" name="password" id="password" />
-			        </div>
-			        <div>
-				        <input type="submit" value="Log in" />
-				        <a href="http://localhost/MegaProject/Body/Websites/HomePage/HomePage.php">Register</a>
+				        <input style="float: right; font-size: 14pt" type="submit" value="Sign in" />
 			        </div>
 		        </form><!-- form -->
 
--- Body/Websites/SignUp/SignUp.php
+++ Body/Websites/SignUp/SignUp.php
@@ -24,15 +24,44 @@ if (isset($_SESSION['msg'])) {
         <section id="content">
             <form method="POST" action="SignUp.php">
                 <h1>Sign-Up</h1>
-                <div>
-                    <input type="text" placeholder="Username" id="username" name="username" />
+                <div class="Input">
+                    <p>Username:</p>
+                    <input class="Input2" type="text" placeholder="Username" id="username" name="username" />
+                </div>
+                <div class="Input">
+                    <p>Password:</p>
+                    <input class="Input2" type="password" placeholder="Password" name="password" id="password" />
+                </div>
+                <div class="Input">
+                    <p>First Name:</p>
+                    <input class="Input2" type="text" placeholder="Password" name="firstname" id="fname" />
+                </div>
+                <div class="Input">
+                    <p>Last Name:</p>
+                    <input class="Input2" type="text" placeholder="Last Name" name="lastname" id="lname" />
+                </div>
+                <div class="Input">
+                    <p>Gender:</p>
+                    <input class="Input2" type="text" placeholder="Gender" name="gender" id="gender" />
+                </div>
+                <div class="Input">
+                    <p>Date Of Birth:</p>
+                    <input class="Input2" type="date" placeholder="Date Of Birth" name="dob" id="dob" />
+                </div>
+                <div class="Input">
+                    <p>Address:</p>
+                    <input class="Input2" type="text" placeholder="Address" name="address" id="address" />
+                </div>
+                <div class="Input">
+                    <p>Phone:</p>
+                    <input class="Input2" type="number" placeholder="Phone" name="phone" id="phone" />
+                </div>
+                <div class="Input">
+                    <p>Email:</p>
+                    <input class="Input2" type="text" placeholder="Email" name="email" id="email" />
                 </div>
                 <div>
-                    <input type="password" placeholder="Password" name="password" id="password" />
-                </div>
-                <div>
-                    <input type="submit" value="Log in" />
-                    <a href="http://localhost/MegaProject/Body/Websites/HomePage/HomePage.php">Register</a>
+                    <input style="float: right; font-size: 14pt" type="submit" value="Sign up" />
                 </div>
             </form><!-- form -->
 
