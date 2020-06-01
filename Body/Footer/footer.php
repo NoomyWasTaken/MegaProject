a118e0c74ee2950f01c0faa4f884c7fbfa9a9dfe
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-01 20:38:07 +0200

    target="_blank"

--- Body/Footer/footer.php
+++ Body/Footer/footer.php
@@ -2,10 +2,10 @@
 <footer>
     &copy; Copyright 2020 PAWS
     <div>
-        <a href="https://www.facebook.com/"><img src="../../../Images/fbicon.png" alt="fbicon"></a>
-        <a href="https://www.twitter.com/explore"><img src="../../../Images/twittericon.png" alt="twtricon""></a>
-        <a href="https://www.instagram.com/"><img src="../../../Images/instaicon.png" alt="instaicon"></a>
-        <a href="https://www.tiktok.com/en/"><img src="../../../Images/tiktokicon.png" alt="tiktokicon"></a>
-        <a href="https://www.linkedin.com/"><img src="../../../Images/linkedinicon.png" alt="lninicon"></a>
+        <a href="https://www.facebook.com/" target="_blank"><img src="../../../Images/fbicon.png" alt="fbicon"></a>
+        <a href="https://www.twitter.com/explore" target="_blank"><img src="../../../Images/twittericon.png" alt="twtricon"></a>
+        <a href="https://www.instagram.com/" target="_blank"><img src="../../../Images/instaicon.png" alt="instaicon"></a>
+        <a href="https://www.tiktok.com/en/" target="_blank"><img src="../../../Images/tiktokicon.png" alt="tiktokicon"></a>
+        <a href="https://www.linkedin.com/" target="_blank"><img src="../../../Images/linkedinicon.png" alt="lninicon"></a>
     </div>
 </footer>
