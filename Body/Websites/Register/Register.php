e3554a3f499af1a100d2e62ed2a38d8757d5181f
TheRealDemmy <Dendimir101@yahoo.com>
2020-06-01 21:08:56 +0200

    Removed Sign Up from right.

--- Body/Websites/Register/Register.php
+++ Body/Websites/Register/Register.php
@@ -23,7 +23,7 @@
 	    <div class="SignIn">
 	        <section id="content">
 		        <form method="POST" action="login.php">
-			        <h1>Login Form</h1>
+			        <h1>Sign In</h1>
 			        <div>
 				        <input type="text" placeholder="Username" id="username" name="username" />
 			        </div>
@@ -38,20 +38,6 @@
 
 	        </section><!-- content -->
         </div><!-- container -->
-    <div class="SignIn">
-        <section id="content">
-            <form method="POST" action="login.php">
-                <h1>Login Form</h1>
-                <div>
-                    <input type="text" placeholder="Username" id="username" name="username" />
-                </div>
-                <div>
-                    <input type="password" placeholder="Password" name="password" id="password" />
-                </div>
-                <div>
-                    <input type="submit" value="Log in" />
-                    <a href="http://localhost/MegaProject/Body/Websites/HomePage/HomePage.php">Register</a>
-                </div>
             </form><!-- form -->
 
         </section><!-- content -->
