09d9a742e1022ecbacd4f05a51ba1ea9d474338b
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-04 00:43:12 +0200

    Changing Header

--- Body/Header/header.php
+++ Body/Header/header.php
@@ -1,28 +1,124 @@
 <title><?php echo $pageTitle; ?></title>
 <link rel="stylesheet" type="text/css" href="header.css">
 <header>
-    <div class="yes">
-        <a href="http://localhost/MegaProject/Body/Websites/HomePage/HomePage.php"><img id="logo" src="../../../Images/logo-test2.png" alt="logo" style="height: 200px;"></a>
-        <br>
-        <nav>
-            <div class="dropdown">
-                <a id="dropbtn" href="#">Animals</a>
-                <div class="dropdown-content">
-                    <a href="#">Dogs</a>
-                    <br>
-                    <a href="#">Cats</a>
-                    <br>
-                    <a href="#">Birds</a>
-                    <br>
-                    <a href="#">Other</a>
+    <div id="structure">
+        <div class="bone">
+
+            <div class="s1"></div>
+            <div class="s2"></div>
+            <div class="s3"></div>
+            <div class="s4"></div>
+
+            <div class="centerbone">
+                <div class="clean">
+
+                    <div class="logo">
+                        <img src="../../../Images/logo-test2.png" alt="logo" style="width: 200px">
+                    </div>
+
+                    <div class="dropdown">
+                        <div class="container" onclick="myFunction(this)">
+                            <div class="bar1"></div>
+                            <div class="bar2"></div>
+                            <div class="bar3"></div>
+
+                            <div id="myDropdown" class="dropdown-content">
+
+                                <h2>Home Page</h2>
+                                <hr>
+
+                                <button class="bone_btn">
+
+                                    <div class="c1"></div>
+                                    <div class="c2"></div>
+                                    <div class="c3"></div>
+                                    <div class="c4"></div>
+                                    <div class="b1">
+                                        <div class="b2">
+                                            <a href="#Animals">Animals</a>
+                                        </div>
+                                    </div>
+                                </button>
+
+                                <button class="bone_btn">
+                                    <div class="c1"></div>
+                                    <div class="c2"></div>
+                                    <div class="c3"></div>
+                                    <div class="c4"></div>
+                                    <div class="b1">
+                                        <div class="b2">
+                                            <a href="#Adopt">Adopt</a>
+                                        </div>
+                                    </div>
+                                </button>
+
+                                <button class="bone_btn">
+                                    <div class="c1"></div>
+                                    <div class="c2"></div>
+                                    <div class="c3"></div>
+                                    <div class="c4"></div>
+                                    <div class="b1">
+                                        <div class="b2">
+                                            <a href="#About Us">About Us</a>
+                                        </div>
+                                    </div>
+                                </button>
+
+                                <button class="bone_btn">
+                                    <div class="c1"></div>
+                                    <div class="c2"></div>
+                                    <div class="c3"></div>
+                                    <div class="c4"></div>
+                                    <div class="b1">
+                                        <div class="b2">
+                                            <a href="#Sign-in">Sign-in</a>
+                                        </div>
+                                    </div>
+                                </button>
+
+                                <button class="bone_btn">
+                                    <div class="c1"></div>
+                                    <div class="c2"></div>
+                                    <div class="c3"></div>
+                                    <div class="c4"></div>
+                                    <div class="b1">
+                                        <div class="b2">
+                                            <a href="#Sign-up">Sign-up</a>
+                                        </div>
+                                    </div>
+                                </button>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <script>
+                        function myFunction(x) {
+                            x.classList.toggle("change");
+                            document.getElementById("myDropdown").classList.toggle("show");
+                        }
+
+                        window.onclick = function(event) {
+                            if (!event.target.matches('.container')) {
+                                var dropdowns = document.getElementsByClassName("dropdown-content");
+                                var i;
+                                for (i = 0; i < dropdowns.length; i++) {
+                                    var openDropdown = dropdowns[i];
+                                    if (openDropdown.classList.contains('show')) {
+                                        openDropdown.classList.remove('show');
+                                    }
+                                }
+                            }
+                        }
+
+                    </script>
+
+
                 </div>
             </div>
-            <a href="http://localhost/MegaProject/Body/Websites/Adopt/Adopt.php">Adopt</a>
-            <a href="http://localhost/MegaProject/Body/Websites/AboutUs/AboutUs.php">About Us</a>
-            <a href="http://localhost/MegaProject/Body/Websites/Contact/Contact.php">Contact</a>
-            <a href="http://localhost/MegaProject/Body/Websites/SignIn/SignIn.php">Sign-in</a>
-            <a href="http://localhost/MegaProject/Body/Websites/SignUp/SignUp.php">Sign-up</a>
-        </nav>
+
+        </div>
     </div>
 </header>
 <div style="clear: both"></div>
