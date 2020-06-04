bae20f97892138ce2e32df5ba1667fadedfcb09d
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-04 20:26:13 +0200

    Header Change To Body From Main

--- Body/Header/header.php
+++ Body/Header/header.php
@@ -13,7 +13,7 @@
                 <div class="clean">
 
                     <div class="logo">
-                        <a href="#HomePage"> <img src="../../../Images/logo-test2.png" alt="logo" style="width: 200px"> </a>
+                        <a href="http://localhost/MegaProject/Body/Websites/HomePage/HomePage.php"> <img src="../../../Images/logo-test2.png" alt="logo" style="width: 200px"> </a>
                     </div>
 
                     <div class="dropdown">
