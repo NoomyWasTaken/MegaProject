b981d6bc8ed174a888eb63420a542e8e403ce309
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-07 12:09:42 +0200

    Donation Page + Body Layout making overflow: overlay; for scrollbar

--- Body/Websites/Donate/Donate.php
+++ Body/Websites/Donate/Donate.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" type="text/css" href="../../Header/header.css">
     <link rel="stylesheet" type="text/css" href="../../BodyLayout/BodyLayout.css">
     <link rel="stylesheet" type="text/css" href="../../Footer/footer.css">
-    <link rel="stylesheet" type="text/css" href="SignIn.css">
+    <link rel="stylesheet" type="text/css" href="Donate.css">
 
 
 <body>
@@ -26,19 +26,22 @@ include('../../Header/header.php'); ?>
                 <h1>Donate</h1>
                 <h2>All donations are appreciated :)!</h2>
                 <div class="Input">
-                    <label class="Input2" for="Gender">Donate:</label>
-                    <select name="Gender" id="gender">
-                        <option value="             ">             </option>
-                        <option value="Male">  Male  </option>
-                        <option value="saab">  Female  </option>
-                        <option value="Other">  Other  </option>
+                    <label class="Input2" for="Option">Method:</label>
+                    <select onchange="method(this);">
+                        <option id="mc" value="mastercard">  Mastercard  </option>
+                        <option id="v" value="visa">  Visa  </option>
+                        <option id="ae" value="americanexpress">  American Express  </option>
                     </select>
                 </div>
-                <div class="Input">
-                    <p>First Name:</p>
-                    <input class="Input2" type="text" placeholder="First Name" id="fname" name="fname" />
+                <div id="ifSelectedMC" style="display: block">
+                    <p>Card Number:</p>
+                    <input class="Input2" type="number" placeholder="XXXX XXXX XXXX XXXX" id="cnum" name="cnum" />
                 </div>
-                <div class="Input">
+                <div id="ifSelectedV" style="display: none">
+                    <p>Last Name:</p>
+                    <input class="Input2" type="text" placeholder="Last Name" name="lname" id="lname" />
+                </div>
+                <div id="ifSelectedAE" style="display: none">
                     <p>Last Name:</p>
                     <input class="Input2" type="text" placeholder="Last Name" name="lname" id="lname" />
                 </div>
@@ -46,6 +49,31 @@ include('../../Header/header.php'); ?>
                     <input style="float: right; font-size: 14pt" type="submit" value="Donate" />
                 </div>
             </form>
+
+            <script>
+            function method(that) {
+                if (that.value == "mastercard") {
+                    alert("check");
+                    document.getElementById("ifSelectedMC").style.display = "block";
+                    document.getElementById("ifSelectedV").style.display = "none";
+                    document.getElementById("ifSelectedAE").style.display = "none";
+                } else if (that.value == "visa") {
+                    alert("check");
+                    document.getElementById("ifSelectedMC").style.display = "none";
+                    document.getElementById("ifSelectedV").style.display = "block";
+                    document.getElementById("ifSelectedAE").style.display = "none";            }
+                } else if (that.value == "americanexpress") {
+                    alert("check");
+                    document.getElementById("ifSelectedMC").style.display = "none";
+                    document.getElementById("ifSelectedV").style.display = "none";
+                    document.getElementById("ifSelectedAE").style.display = "block";
+                } else {
+                    document.getElementById("ifSelectedMC").style.display = "none";
+                    document.getElementById("ifSelectedV").style.display = "none";
+                    document.getElementById("ifSelectedAE").style.display = "none";
+                }
+            }
+            </script>
 
         </section>
     </div>
