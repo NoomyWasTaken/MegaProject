c0cc8067f797488d13cc849e4a1917591dc0c70b
TheRealDemmy <Dendimir101@yahoo.com>
2020-06-08 22:13:47 +0200

    Top part of homepage donezo

--- Body/Websites/HomePage/HomePage.php
+++ Body/Websites/HomePage/HomePage.php
@@ -69,40 +69,21 @@ include('../../Header/header.php'); ?>
                     </div>
 
                     <div id="DonateWindow">
-                        <h2>Looking to adopt?</h2>
+                        <h2>Donate today!</h2>
                         <br>
-                        <p>Help find the animals we have a new home today!</p>
+                        <p>Any donation is appreciated!</p>
                         <a href="//widgets.justgiving.com/Button/Redirect?p=eyJJZCI6IjNkMmNiYTk1LWRiNGEtNDE3Ny1hNmUzLWI4ZjQyOGViOTEyMCIsIkNoYXJpdHlJZCI6MTg0ODYxLCJTaXplIjoicyIsIlJlZmVyZW5jZSI6Im15cGlwaWlzdHNjaHdlciIsIlR5cGUiOiJEb25hdGUifQ=="
                            onclick="window.open('//widgets.justgiving.com/Button/Redirect?p=eyJJZCI6IjNkMmNiYTk1LWRiNGEtNDE3Ny1hNmUzLWI4ZjQyOGViOTEyMCIsIkNoYXJpdHlJZCI6MTg0ODYxLCJTaXplIjoicyIsIlJlZmVyZW5jZSI6Im15cGlwaWlzdHNjaHdlciIsIlR5cGUiOiJEb25hdGUifQ==',
                          'newwindow',
                          'width=500,height=470');
               return false;"
-                        > <img src='../../../Images/donatewhite.jpg' onmouseover="this.src='../../../Images/donatered.jpg';" onmouseout="this.src='../../../Images/donatewhite.jpg';" /> </a>
+                            > <img src='../../../Images/donatewhite.jpg' onmouseover="this.src='../../../Images/donatered.jpg';" onmouseout="this.src='../../../Images/donatewhite.jpg';" /> </a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="Donation_Adoption">
-                <a href="/../MegaProject/Body/Websites/Adopt/Adopt.php"> <img src='../../../Images/AdoptionBlackNew.png' onmouseover="this.src='../../../Images/adoptionred.png';" onmouseout="this.src='../../../Images/AdoptionBlackNew.png';" /> </a>
-                <a href="//widgets.justgiving.com/Button/Redirect?p=eyJJZCI6IjNkMmNiYTk1LWRiNGEtNDE3Ny1hNmUzLWI4ZjQyOGViOTEyMCIsIkNoYXJpdHlJZCI6MTg0ODYxLCJTaXplIjoicyIsIlJlZmVyZW5jZSI6Im15cGlwaWlzdHNjaHdlciIsIlR5cGUiOiJEb25hdGUifQ=="
-                   onclick="window.open('//widgets.justgiving.com/Button/Redirect?p=eyJJZCI6IjNkMmNiYTk1LWRiNGEtNDE3Ny1hNmUzLWI4ZjQyOGViOTEyMCIsIkNoYXJpdHlJZCI6MTg0ODYxLCJTaXplIjoicyIsIlJlZmVyZW5jZSI6Im15cGlwaWlzdHNjaHdlciIsIlR5cGUiOiJEb25hdGUifQ==',
-                         'newwindow',
-                         'width=500,height=470');
-              return false;"
-                > <img src='../../../Images/donatewhite.jpg' onmouseover="this.src='../../../Images/donatered.jpg';" onmouseout="this.src='../../../Images/donatewhite.jpg';" /> </a>
-<!--                <a href="https://www.paypal.com/webapps/shoppingcart?flowlogging_id=7fc2c48a0aad5&mfid=1591401347799_7fc2c48a0aad5#/checkout/openButton"-->
-<!--                   onclick="window.open('https://www.paypal.com/webapps/shoppingcart?flowlogging_id=37a6c6fce93ce&mfid=1591463404579_37a6c6fce93ce#/checkout/openButton',-->
-<!--                         'newwindow',-->
-<!--                         'width=500,height=470');-->
-<!--              return false;"-->
-<!--                > <img src='../../../Images/donatewhite.jpg' onmouseover="this.src='../../../Images/donatered.jpg';" onmouseout="this.src='../../../Images/donatewhite.jpg';" /> </a>-->
-<!--                <a href="//widgets.justgiving.com/Button/Redirect?p=eyJJZCI6IjNkMmNiYTk1LWRiNGEtNDE3Ny1hNmUzLWI4ZjQyOGViOTEyMCIsIkNoYXJpdHlJZCI6MTg0ODYxLCJTaXplIjoicyIsIlJlZmVyZW5jZSI6Im15cGlwaWlzdHNjaHdlciIsIlR5cGUiOiJEb25hdGUifQ=="><img src="//widgets.justgiving.com/Button?p=eyJJZCI6IjNkMmNiYTk1LWRiNGEtNDE3Ny1hNmUzLWI4ZjQyOGViOTEyMCIsIkNoYXJpdHlJZCI6MTg0ODYxLCJTaXplIjoicyIsIlJlZmVyZW5jZSI6Im15cGlwaWlzdHNjaHdlciIsIlR5cGUiOiJEb25hdGUifQ==" /></a>-->
-<!--                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">-->
-<!--                    <input type="hidden" name="cmd" value="_s-xclick">-->
-<!--                    <input type="hidden" name="hosted_button_id" value="AUN7X7VDVPC5A">-->
-<!--                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">-->
-<!--                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">-->
-<!--                </form>-->
+
+
             </div>
         </main>
     </div>
