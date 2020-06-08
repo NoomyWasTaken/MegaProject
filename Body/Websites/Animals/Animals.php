518438ba92c59f9a32185b724b9e00274531f37d
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-09 01:48:52 +0200

    Animal Slideshow

--- Body/Websites/Animals/Animals.php
+++ Body/Websites/Animals/Animals.php
@@ -24,75 +24,119 @@ include('../../Header/header.php'); ?>
     <div id="container">
         <aside id="navigation">
             <h2 id="dog" onclick="dogFunction()">Dogs</h2>
+            <br>
             <h2 id="cat" onclick="catFunction()">Cats</h2>
+            <br>
             <h2 id="bird" onclick="birdFunction()">Birds</h2>
+            <br>
             <h2 id="rodent" onclick="rodentFunction()">Rodents</h2>
+            <br>
             <h2 id="other" onclick="otherFunction()">Other</h2>
         </aside>
 
         <script>
             function dogFunction() {
-                document.getElementById("doggy").style.display = "block";
-                document.getElementById("catty").style.display = "none";
-                document.getElementById("birdy").style.display = "none";
-                document.getElementById("rody").style.display = "none";
-                document.getElementById("othy").style.display = "none";
+                document.getElementById("doge").style.display = "block";
+                document.getElementById("pus").style.display = "none";
+                document.getElementById("birb").style.display = "none";
+                document.getElementById("road").style.display = "none";
+                document.getElementById("melman").style.display = "none";
             }
             function catFunction() {
-                document.getElementById("doggy").style.display = "none";
-                document.getElementById("catty").style.display = "block";
-                document.getElementById("birdy").style.display = "none";
-                document.getElementById("rody").style.display = "none";
-                document.getElementById("othy").style.display = "none";
+                document.getElementById("doge").style.display = "none";
+                document.getElementById("pus").style.display = "block";
+                document.getElementById("birb").style.display = "none";
+                document.getElementById("road").style.display = "none";
+                document.getElementById("melman").style.display = "none";
             }
             function birdFunction() {
-                document.getElementById("doggy").style.display = "none";
-                document.getElementById("catty").style.display = "none";
-                document.getElementById("birdy").style.display = "block";
-                document.getElementById("rody").style.display = "none";
-                document.getElementById("othy").style.display = "none";
+                document.getElementById("doge").style.display = "none";
+                document.getElementById("pus").style.display = "none";
+                document.getElementById("birb").style.display = "block";
+                document.getElementById("road").style.display = "none";
+                document.getElementById("melman").style.display = "none";
             }
             function rodentFunction() {
-                document.getElementById("doggy").style.display = "none";
-                document.getElementById("catty").style.display = "none";
-                document.getElementById("birdy").style.display = "none";
-                document.getElementById("rody").style.display = "block";
-                document.getElementById("othy").style.display = "none";
+                document.getElementById("doge").style.display = "none";
+                document.getElementById("pus").style.display = "none";
+                document.getElementById("birb").style.display = "none";
+                document.getElementById("road").style.display = "block";
+                document.getElementById("melman").style.display = "none";
             }
             function otherFunction() {
-                document.getElementById("doggy").style.display = "none";
-                document.getElementById("catty").style.display = "none";
-                document.getElementById("birdy").style.display = "none";
-                document.getElementById("rody").style.display = "none";
-                document.getElementById("othy").style.display = "block";
+                document.getElementById("doge").style.display = "none";
+                document.getElementById("pus").style.display = "none";
+                document.getElementById("birb").style.display = "none";
+                document.getElementById("road").style.display = "none";
+                document.getElementById("melman").style.display = "block";
             }
         </script>
 
-        <div id="doggy">
-            <article id="doggy1">
-                <img src="../../../Images/cycle8.png" alt="placeholder">
-            </article>
+        <div id="doge" class="w3-content w3-display-container">
+            <img class="mySlides1" src="../../../Images/cycle1.png" style="width:100%">
+            <img class="mySlides1" src="../../../Images/cycle2.png" style="width:100%">
+            <img class="mySlides1" src="../../../Images/cycle3.png" style="width:100%">
+            <img class="mySlides1" src="../../../Images/cycle4.png" style="width:100%">
+            <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 0)">&#10094;</button>
+            <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 0)">&#10095;</button>
         </div>
-        <div id="catty" style="display: none">
-            <article id="catty1">
-                <img src="../../../Images/cycle3.png" alt="placeholder">
-            </article>
+
+        <div id="pus" class="w3-content w3-display-container" style="display: none">
+            <img class="mySlides2" src="../../../Images/cycle5.png" style="width:100%">
+            <img class="mySlides2" src="../../../Images/cycle6.png" style="width:100%">
+            <img class="mySlides2" src="../../../Images/cycle7.png" style="width:100%">
+            <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 1)">&#10094;</button>
+            <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 1)">&#10095;</button>
         </div>
-        <div id="birdy" style="display: none">
-            <article id="birdy1">
-                <img src="../../../Images/cycle1.png" alt="placeholder">
-            </article>
+
+        <div id="birb" class="w3-content w3-display-container" style="display: none">
+            <img class="mySlides3" src="../../../Images/cycle1.png" style="width:100%">
+            <img class="mySlides3" src="../../../Images/cycle1.png" style="width:100%">
+            <img class="mySlides3" src="../../../Images/cycle1.png" style="width:100%">
+            <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 1)">&#10094;</button>
+            <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 1)">&#10095;</button>
         </div>
-        <div id="rody" style="display: none">
-            <article id="rody1">
-                <img src="../../../Images/cycle2.png" alt="placeholder">
-            </article>
+
+        <div id="road" class="w3-content w3-display-container" style="display: none">
+            <img class="mySlides4" src="../../../Images/cycle1.png" style="width:100%">
+            <img class="mySlides4" src="../../../Images/cycle1.png" style="width:100%">
+            <img class="mySlides4" src="../../../Images/cycle1.png" style="width:100%">
+            <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 1)">&#10094;</button>
+            <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 1)">&#10095;</button>
         </div>
-        <div id="othy" style="display: none">
-            <article id="othy1">
-                <img src="../../../Images/cycle7.png" alt="placeholder">
-            </article>
+
+        <div id="melman" class="w3-content w3-display-container" style="display: none">
+            <img class="mySlides5" src="../../../Images/cycle1.png" style="width:100%">
+            <img class="mySlides5" src="../../../Images/cycle1.png" style="width:100%">
+            <img class="mySlides5" src="../../../Images/cycle1.png" style="width:100%">
+            <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1, 1)">&#10094;</button>
+            <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1, 1)">&#10095;</button>
         </div>
+
+        <script>
+            var slideIndex = [1,1,1,1,1];
+            var slideId = ["mySlides1", "mySlides2","mySlides3", "mySlides4","mySlides5"]
+            showDivs(1, 0);
+            showDivs(1, 1);
+            showDivs(1, 2);
+            showDivs(1, 3);
+            showDivs(1, 4);
+
+            function plusDivs(n, no) {
+                showDivs(slideIndex[no] += n, no);
+            }
+
+            function showDivs(n, no) {
+                var i;
+                var x = document.getElementsByClassName(slideId[no]);
+                if (n > x.length) {slideIndex[no] = 1}
+                if (n < 1) {slideIndex[no] = x.length}
+                for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";
+                }
+                x[slideIndex[no]-1].style.display = "block";
+            }
+        </script>
     </div>
 </main>
 
