c4f1f9fc5294a83cd79bfbbe8ec1a0570f4dc6a2
muhamedkhatib <mimipalys236@gmail.com>
2020-06-09 14:50:51 +0200

    help

--- Body/Websites/AboutUs/AboutUs.php
+++ Body/Websites/AboutUs/AboutUs.php
@@ -63,7 +63,7 @@ include('../../Header/header.php'); ?>
         </div>
 
         <div class="muhamed">
-            <img src="/../MegaProject/Images/demmy.jpeg" alt="muhamed">
+            <img src="/../MegaProject/Images/muhamed.jpg" alt="muhamed">
             <h2>Muhamed Khatib</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus quam nulla, vel faucibus lorem ultrices nec. Mauris finibus tortor ligula. Fusce faucibus nulla vitae risus elementum, ac lacinia nibh maximus. Integer faucibus quam ut scelerisque pulvinar. Donec pharetra nibh eget commodo accumsan. Donec at nisl nec justo condimentum rutrum. Aliquam lacinia, neque et varius fringilla, ex felis dignissim diam, vel fermentum dui massa sed nulla. Fusce congue orci at urna vulputate euismod.</p>
         </div>
