b0ffc99faeeea22f77ee1933227adbfdef4b3104
muhamedkhatib <mimipalys236@gmail.com>
2020-06-09 12:51:39 +0200

    abut us

--- Body/Websites/AboutUs/AboutUs.php
+++ Body/Websites/AboutUs/AboutUs.php
@@ -1,25 +1,121 @@
-<?php?>
 <!DOCTYPE html>
 <html>
+
 <head>
+
     <title>About Us</title>
     <link rel="stylesheet" type="text/css" href="../../Header/header.css">
     <link rel="stylesheet" type="text/css" href="../../BodyLayout/BodyLayout.css">
     <link rel="stylesheet" type="text/css" href="../../Footer/footer.css">
     <link rel="stylesheet" type="text/css" href="AboutUs.css">
+
+</head>
+
 <body>
+
 <?php
 $pageTitle = 'About Us';
 include('../../Header/header.php'); ?>
 <main>
-    <div id="About Us">
+    <div id="AboutUs">
+            <h1 id="ABh1">Our organization</h1>
 
         <div class="ourfamily">
         <img src="/../MegaProject/Images/ourFamily.jpg" alt="our family">
+            <br>
+            <h1>Our Family</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus quam nulla, vel faucibus lorem ultrices nec. Mauris finibus tortor ligula. Fusce faucibus nulla vitae risus elementum, ac lacinia nibh maximus. Integer faucibus quam ut scelerisque pulvinar. Donec pharetra nibh eget commodo accumsan. Donec at nisl nec justo condimentum rutrum. Aliquam lacinia, neque et varius fringilla, ex felis dignissim diam, vel fermentum dui massa sed nulla. Fusce congue orci at urna vulputate euismod.
+
+                Nullam non tincidunt est. Ut in tempor augue. Morbi ornare auctor nisi et vehicula. Curabitur nunc lacus, elementum sed tortor sit amet, rhoncus hendrerit ex. Sed rutrum sapien vel mi euismod, id lacinia tortor dictum. Sed varius mattis diam, quis tincidunt erat sollicitudin et. Cras nec maximus elit. Nunc porttitor at neque eu congue. Suspendisse et leo sed ligula blandit rutrum. Sed faucibus malesuada nisi, vitae maximus ligula feugiat quis. Nullam varius risus quis tellus ultrices blandit. In convallis vel tellus ac suscipit.
+
+                Etiam dolor felis, egestas eget tincidunt in, lacinia a tortor. Sed leo tortor, vulputate eu nibh eget, commodo placerat purus. Ut quis facilisis nulla. Nam mattis luctus odio, nec pretium purus faucibus ut. Nullam in fringilla nisl, nec suscipit elit. Cras porta accumsan feugiat. Cras vitae ex et neque commodo tempus ac non ligula. Vestibulum aliquam ultricies sodales. Sed gravida tellus eu velit consequat, id maximus justo volutpat. Donec id elementum augue, eu lobortis nunc. Suspendisse quis libero quis nisl lacinia convallis. Etiam euismod consequat metus mollis scelerisque. Donec fermentum mi ut diam ultrices dapibus.</p>
         </div>
+
+        <div class="ourfamily2">
+
+         <section>
+        <img src="/../MegaProject/Images/walking%20a%20giraffe.jpg" alt="a man walking a giraffe">
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus quam nulla, vel faucibus lorem ultrices nec. Mauris finibus tortor ligula. Fusce faucibus nulla vitae risus elementum, ac lacinia nibh maximus. Integer faucibus quam ut scelerisque pulvinar. Donec pharetra nibh eget commodo accumsan. Donec at nisl nec justo condimentum rutrum. Aliquam lacinia, neque et varius fringilla, ex felis dignissim diam, vel fermentum dui massa sed nulla. Fusce congue orci at urna vulputate euismod.</p>
+         </section>
+
+            <section>
+                <img src="/../MegaProject/Images/perizubebrate.jpg" alt="dog getting teeth cleaned">
+                <p>Nullam non tincidunt est. Ut in tempor augue. Morbi ornare auctor nisi et vehicula. Curabitur nunc lacus, elementum sed tortor sit amet, rhoncus hendrerit ex. Sed rutrum sapien vel mi euismod, id lacinia tortor dictum. Sed varius mattis diam, quis tincidunt erat sollicitudin et. Cras nec maximus elit. Nunc porttitor at neque eu congue. Suspendisse et leo sed ligula blandit rutrum. Sed faucibus malesuada nisi, vitae maximus ligula feugiat quis. Nullam varius risus quis tellus ultrices blandit. In convallis vel tellus ac suscipit.</p>
+            </section>
+
+        </div>
+
+        <hr id="AUhr">
+
+        <h1 id="ABh1B">Our members</h1>
+
+        <div class="denim">
+            <img src="/../MegaProject/Images/demmy.jpeg" alt="denim">
+            <h2>Denim Hasan-Basic</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus quam nulla, vel faucibus lorem ultrices nec. Mauris finibus tortor ligula. Fusce faucibus nulla vitae risus elementum, ac lacinia nibh maximus. Integer faucibus quam ut scelerisque pulvinar. Donec pharetra nibh eget commodo accumsan. Donec at nisl nec justo condimentum rutrum. Aliquam lacinia, neque et varius fringilla, ex felis dignissim diam, vel fermentum dui massa sed nulla. Fusce congue orci at urna vulputate euismod.</p>
+        </div>
+
+        <div class="neumen">
+            <img src="/../MegaProject/Images/noomy.jpeg" alt="neumen">
+            <h2>Neumen AL-Khalil</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus quam nulla, vel faucibus lorem ultrices nec. Mauris finibus tortor ligula. Fusce faucibus nulla vitae risus elementum, ac lacinia nibh maximus. Integer faucibus quam ut scelerisque pulvinar. Donec pharetra nibh eget commodo accumsan. Donec at nisl nec justo condimentum rutrum. Aliquam lacinia, neque et varius fringilla, ex felis dignissim diam, vel fermentum dui massa sed nulla. Fusce congue orci at urna vulputate euismod.</p>
+        </div>
+
+        <div class="muhamed">
+            <img src="/../MegaProject/Images/demmy.jpeg" alt="muhamed">
+            <h2>Muhamed Khatib</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum maximus quam nulla, vel faucibus lorem ultrices nec. Mauris finibus tortor ligula. Fusce faucibus nulla vitae risus elementum, ac lacinia nibh maximus. Integer faucibus quam ut scelerisque pulvinar. Donec pharetra nibh eget commodo accumsan. Donec at nisl nec justo condimentum rutrum. Aliquam lacinia, neque et varius fringilla, ex felis dignissim diam, vel fermentum dui massa sed nulla. Fusce congue orci at urna vulputate euismod.</p>
+        </div>
+
+        <hr id="AUhr">
+
+        <h1 id="ABh1B">Our Partners</h1>
+
+        <div class="org">
+
+        <div class="leftorg">
+            <section>
+            <img src="/../MegaProject/Images/wwf.png" alt="WWF">
+                <h2>WWF</h2>
+                <p></p>
+            </section>
+
+            <section>
+            <img src="/../MegaProject/Images/p4u.png" alt="Pets4Us" >
+                <h2>Pets4Us</h2>
+                <p></p>
+            </section>
+        </div>
+
+            <br>
+
+        <div class="rightorg">
+            <section>
+            <img src="/../MegaProject/Images/reddog.png" alt="A.R.T&PAWS" >
+                <h2>A.R.T&PAWS</h2>
+                <p></p>
+
+            </section>
+
+            <section>
+            <img src="/../MegaProject/Images/fon.png" alt="Friends of nature">
+                <h2>Friends of Nature</h2>
+                <p></p>
+            </section>
+        </div>
+
+        </div>
+
+
 
     </div>
 </main>
+
+<div style="clear: both"></div>
+
+
 <?php include('../../Footer/footer.php'); ?>
+
 </body>
 </html>
