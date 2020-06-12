23c02e6e6faa83fb541ac5a3fe1a777e36e1b088
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-12 16:46:24 +0200

    PLEASE 16

--- Body/Websites/HomePage/HomePage.php
+++ Body/Websites/HomePage/HomePage.php
@@ -2,7 +2,7 @@
 session_start();
 $conn = oci_connect('admin', 'Mimiplays23610', 'megaproject_high');
 
-$query = 'SELECT COUNT(*) FROM ANIMAL WHERE IS_ADOPTED = 1';
+$query = 'SELECT COUNT(*) AS "total" FROM ADOPTION_FORM WHERE IS_ADOPTED = 1';
 $stid = oci_parse($conn, $query);
 oci_execute($stid);
 ?>
@@ -81,10 +81,8 @@ include('../../Header/header.php'); ?>
                     <div class="Totals">
                         <h2>All adoptions so far: </h2>
                         <?php
-                            while ($yes = oci_fetch_assoc($stid)) {
-                                echo $yes["is_adopted"];
-                            }
-
+                                $yes = oci_fetch_assoc($stid);
+                                echo $yes["total"];
                         ?>
                     </div>
                     <br><br>
