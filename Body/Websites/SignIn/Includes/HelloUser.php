7ef0e060089bb0190bee939576c2c9772eededda
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-13 03:29:36 +0200

    Fixing Sign in FINAL V7

--- Body/Websites/SignIn/Includes/HelloUser.php
+++ Body/Websites/SignIn/Includes/HelloUser.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+$conn = oci_connect('ADMIN', 'Mimiplays23610', 'megaproject_high');
+
 if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
     echo "$username";
