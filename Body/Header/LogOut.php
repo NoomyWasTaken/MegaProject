f79d9ef0d4b1d3096fcebc5cbfc660c305000178
TheRealDemmy <Dendimir101@yahoo.com>
2020-06-13 16:22:46 +0200

    Created LogOut option test 6

--- Body/Header/LogOut.php
+++ Body/Header/LogOut.php
@@ -1,9 +1,8 @@
 <?php
-if (isset($_SESSION['username'])) {
     session_start();
 
     $conn = oci_connect('admin', 'Mimiplays23610', 'megaproject_high');
     session_destroy();
     $_SESSION = [];
-}
+
 header('Location: /../MegaProject/Body/Websites/HomePage/HomePage.php');
