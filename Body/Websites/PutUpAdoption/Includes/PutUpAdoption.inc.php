daa37ccb6711730d3dd5443acaa03faea0a54738
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-13 19:01:32 +0200

    PutUpAdoption.php and PutUpAdoption.inc.php fix 3

--- Body/Websites/PutUpAdoption/Includes/PutUpAdoption.inc.php
+++ Body/Websites/PutUpAdoption/Includes/PutUpAdoption.inc.php
@@ -51,7 +51,7 @@ if (isset($_POST['put-up-submit'])) {
             $fileDestination = '../../../../Images/Adopt/'.$fileNewName;
             move_uploaded_file($fileTmpName, $fileDestination);
 
-            $sql = 'INSERT INTO animal (aname, age, height, weight, species, shelter_id, is_adopted, gender, image) VALUES (:aname, :age, :height, :weight, :species, :shelter_id, :is_adopted, :gender, :image)';
+            $sql = 'INSERT INTO animal (aname, age, height, weight, species, shelter_id, gender, image) VALUES (:aname, :age, :height, :weight, :species, :shelter_id, :gender, :image)';
             $insert = oci_parse($conn, $sql);
 
             oci_bind_by_name($insert, ':aname', $aname);
@@ -60,11 +60,39 @@ if (isset($_POST['put-up-submit'])) {
             oci_bind_by_name($insert, ':weight', $weight);
             oci_bind_by_name($insert, ':species', $species);
             oci_bind_by_name($insert, ':shelter_id', $shelter_id);
-            oci_bind_by_name($insert, ':is_adopted', $is_adopted);
             oci_bind_by_name($insert, ':gender', $gender);
             oci_bind_by_name($insert, ':image', $fileNewName);
 
             oci_execute($insert);
+
+            $sql2 = 'SELECT * FROM breed WHERE breed_name = :breedname';
+            $r = oci_parse($conn, $sql2);
+            oci_bind_by_name($r, ':breedname', $breed);
+            oci_execute($r);
+            if (oci_num_rows($r)===0) {
+                $sql10 = 'INSERT INTO breed (breed_name) VALUES (:breedname)';
+                $r10 = oci_parse($conn, $sql10);
+                oci_bind_by_name($r10, ':breedname', $breed);
+                oci_execute($r10);
+            }
+            $sql3 = 'SELECT animal_id FROM animal WHERE aname = :aname, age = :age';
+            $sql4 = 'SELECT breed_id FROM breed WHERE breed_name = :breedname';
+            $r2 = oci_parse($conn, $sql3);
+            $r3 = oci_parse($conn, $sql4);
+            oci_bind_by_name($r2, ':aname', $aname);
+            oci_bind_by_name($r3, ':breedname', $breed);
+            oci_execute($r2);
+            oci_execute($r3);
+            $row = oci_fetch_assoc($r2);
+            $row2 = oci_fetch_assoc($r3);
+            $aid = $row['ANIMAL_ID'];
+            $bid = $row2['BREED_ID'];
+            $sql5 = 'INSERT INTO animal_breed (animal_id, breed_id) VALUES (:aid, :bid)';
+            $insert2 = oci_parse($conn, $sql5);
+            oci_bind_by_name($insert2, ':aid', $aid);
+            oci_bind_by_name($insert2, ':bid', $bid);
+            oci_execute($insert2);
+
             header("Location: ../../HomePage/HomePage.php?success");
         }
     }
