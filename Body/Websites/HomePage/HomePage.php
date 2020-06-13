e888b0aaf1426dd780f30f200fbe20035ad550e0
TheRealDemmy <Dendimir101@yahoo.com>
2020-06-13 17:58:01 +0200

    DOING VOLUNTEERS 3

--- Body/Websites/HomePage/HomePage.php
+++ Body/Websites/HomePage/HomePage.php
@@ -29,7 +29,7 @@ $pageTitle = 'Home Page';
 include('../../Header/header.php'); ?>
 <div id="page-container">
     <div id="wrapper">
-        <div>
+        <main>
             <div class="imgcycle1" style="background-color: #EFE4B0">
                 <img class="elementToFadeInAndOut" src="../../../Images/cycle1.png" alt="image">
                 <img class="elementToFadeInAndOut" src="../../../Images/cycle2.png" alt="image">
@@ -136,9 +136,10 @@ include('../../Header/header.php'); ?>
             </div>
             <hr id="hrline">
             <div class="UnderLine">
-                <h1>I want to help, but not adopt!</h1>
+                <h1>I've never owned a pet, what should I do?</h1>
                 <br>
-                <a href="../Volunteer/Volunteer.php">Volunteer</a>
+                <a href="../Volunteer/Volunteer.php"
+                <p>Don't worry. Plenty of people have yet to ever experience what it means to take care of a pet. It is a responsibility, but an earnest one and a fulfilling one. Our caretakers can show you some of the ropes at your nearest shelter! If you want to find out more about what animal would suit you, click <a href="/../MegaProject/Body/Websites/Animals/Animals.php"> HERE </a> to find out more!</p>
             </div>
         </main>
     </div>
