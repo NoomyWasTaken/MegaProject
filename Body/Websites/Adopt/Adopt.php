a064bc6ed5ca25b65f1ceb8fc113bb4df3a7dd4b
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-14 23:11:00 +0200

    Testing Adopt.php 4

--- Body/Websites/Adopt/Adopt.php
+++ Body/Websites/Adopt/Adopt.php
@@ -17,7 +17,29 @@ include('../../Header/header.php'); ?>
 
 <main>
     <div id="BigWrapper">
-        <?php include("SmallAdopt.php"); ?>
+        <?php while(($row=oci_fetch_assoc($stid)) != false) {
+            echo '<div id="SmallDivContainer">';
+                echo '<img src="../../../Images/cycle10.png">';
+                echo "<h3>Name: $row['ANAME']</h3>";
+                echo '<div id="SidesWrapper">';
+                    echo '<div id="LeftSide">';
+                        echo "<p>Species: $row['SPECIES']</p>";
+                        echo '<br>';
+                        echo "<p>Breed: $row2['BREED_NAME']</p>";
+                        echo "<br>";
+                        echo "<p>Age: $row['AGE']</p>";
+                    echo '</div>';
+                    echo '<div id="RightSide">';
+                        echo "<p>Gender: $row['GENDER']</p>";
+                        echo '<br>';
+                        echo "<p>Height: $row['HEIGHT']</p>";
+                        echo '<br>';
+                        echo "<p>Weight: $row['WEIGHT']</p>";
+                    echo '</div>';
+                echo '</div>';
+                echo '<a href="#">Adopt now!</a>';
+                echo '</div>';
+            ?>
     </div>
 </main>
 
