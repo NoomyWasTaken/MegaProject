02cac2c5a6fbce7d94f92065614fa45bbd9c4223
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-14 23:14:48 +0200

    Testing Adopt.php 5

--- Body/Websites/Adopt/Adopt.php
+++ Body/Websites/Adopt/Adopt.php
@@ -17,29 +17,42 @@ include('../../Header/header.php'); ?>
 
 <main>
     <div id="BigWrapper">
-        <?php while(($row=oci_fetch_assoc($stid)) != false) {
-            echo '<div id="SmallDivContainer">';
-                echo '<img src="../../../Images/cycle10.png">';
-                echo "<h3>Name: $row['ANAME']</h3>";
-                echo '<div id="SidesWrapper">';
-                    echo '<div id="LeftSide">';
-                        echo "<p>Species: $row['SPECIES']</p>";
-                        echo '<br>';
-                        echo "<p>Breed: $row2['BREED_NAME']</p>";
-                        echo "<br>";
-                        echo "<p>Age: $row['AGE']</p>";
-                    echo '</div>';
-                    echo '<div id="RightSide">';
-                        echo "<p>Gender: $row['GENDER']</p>";
-                        echo '<br>';
-                        echo "<p>Height: $row['HEIGHT']</p>";
-                        echo '<br>';
-                        echo "<p>Weight: $row['WEIGHT']</p>";
-                    echo '</div>';
-                echo '</div>';
-                echo '<a href="#">Adopt now!</a>';
-                echo '</div>';
-            ?>
+        <?php while(($row=oci_fetch_assoc($stid)) != false) ?>
+        <div id="SmallDivContainer">
+            <img src="../../../Images/cycle10.png">
+            <h3>Name:
+                <?php echo $row['ANAME']; ?>
+            </h3>
+            <div id="SidesWrapper">
+                <div id="LeftSide">
+                    <p>Species:
+                        <?php echo $row['SPECIES']; ?>
+                    </p>
+                    <br>
+                    <p>Breed:
+                        <?php echo $row2['BREED_NAME']; ?>
+                    </p>
+                    <br>
+                    <p>Age:
+                        <?php echo $row['AGE']; ?>
+                    </p>
+                </div>
+                <div id="RightSide">
+                    <p>Gender:
+                        <?php echo $row['GENDER']; ?>
+                    </p>
+                    <br>
+                    <p>Height:
+                        <?php echo $row['HEIGHT']; ?>
+                    </p>
+                    <br>
+                    <p>Weight:
+                        <?php echo $row['WEIGHT']; ?>
+                    </p>
+                </div>
+            </div>
+            <a href="#">Adopt now!</a>
+        </div>
     </div>
 </main>
 
