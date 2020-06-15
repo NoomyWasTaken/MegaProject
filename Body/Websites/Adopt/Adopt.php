217623b2c7c0a78b7d98919758cdb1351a86d527
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-15 13:39:22 +0200

    Adding Photo If Photo Not Set To Adopt.php 2

--- Body/Websites/Adopt/Adopt.php
+++ Body/Websites/Adopt/Adopt.php
@@ -40,7 +40,7 @@ include('../../Header/header.php'); ?>
 
             echo "<div id='SmallDivContainer'>";
             if ($row['IMAGE'] != NULL) {
-                echo "<img src='".$row['IMAGE']."'>";
+                echo "<img src='../../../Images/Adopt/".$row['IMAGE']."'>";
             } else {
                 echo "<img src='../../../Images/cycle10.png'>";
             }
