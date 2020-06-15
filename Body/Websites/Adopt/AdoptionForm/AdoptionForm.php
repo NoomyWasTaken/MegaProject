b1e1798b85b04beadfb2ab9dbc4b451b9be0ce52
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-16 01:38:41 +0200

    PLEASE GOD LAST

--- Body/Websites/Adopt/AdoptionForm/AdoptionForm.php
+++ Body/Websites/Adopt/AdoptionForm/AdoptionForm.php
@@ -2,22 +2,13 @@
 if (isset($_POST['adopt'])) {
     session_start();
     $conn = oci_connect('admin', 'Mimiplays23610', 'megaproject_high');
-    $d = "18/06/2020";
+    $d = date("yyyy/mm/dd");
     $n = $_POST['n'];
     $a = $_POST['a'];
 
     $sql = "INSERT INTO adoption_form (doa, users_id, animal_id) VALUES ('{$d}','{$n}','{$a}')";
-//    $sql2 = 'UPDATE animal SET is_adopted = 1 WHERE animal_id = :aid2';
     $stid = oci_parse($conn, $sql);
-//    $stid2 = oci_parse($conn, $sql2);
-    oci_bind_by_name($stid, ':doa', $d);
-    oci_bind_by_name($stid, ':uid', $n);
-    oci_bind_by_name($stid, ':aid', $a);
-//    oci_bind_by_name($stid2, ':aid2', $a);
     oci_execute($stid);
-//    oci_execute($stid2);
-//    header("Location: ../../HomePage/HomePage.php?successfullyAdopted");
-    $e = oci_error($stid);
-    echo htmlentities($e['message']);
+    header("Location: ../../HomePage/HomePage.php?successfullyAdopted");
 }
 ?>
