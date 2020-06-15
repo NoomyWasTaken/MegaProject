989317352321d44fccd5295fb86b6e08261093fb
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-16 01:22:01 +0200

    PLEASE GOD LAST

--- Body/Websites/Adopt/AdoptionForm/AdoptionForm.php
+++ Body/Websites/Adopt/AdoptionForm/AdoptionForm.php
@@ -7,15 +7,15 @@ if (isset($_POST['adopt'])) {
     $a = $_POST['a'];
 
     $sql = 'INSERT INTO adoption_form (doa, users_id, animal_id) VALUES (:doa, :uid, :aid)';
-    $sql2 = 'UPDATE animal SET is_adopted = 1 WHERE animal_id = :aid2';
+//    $sql2 = 'UPDATE animal SET is_adopted = 1 WHERE animal_id = :aid2';
     $stid = oci_parse($conn, $sql);
-    $stid2 = oci_parse($conn, $sql2);
+//    $stid2 = oci_parse($conn, $sql2);
     oci_bind_by_name($stid, ':doa', $d);
     oci_bind_by_name($stid, ':uid', $n);
     oci_bind_by_name($stid, ':aid', $a);
-    oci_bind_by_name($stid2, ':aid2', $a);
-    oci_execute($stid2);
+//    oci_bind_by_name($stid2, ':aid2', $a);
     oci_execute($stid);
+//    oci_execute($stid2);
     header("Location: ../../HomePage/HomePage.php?successfullyAdopted");
 }
 ?>
