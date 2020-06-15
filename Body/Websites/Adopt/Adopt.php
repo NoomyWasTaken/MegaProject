729311918ddb6a640c7a2a4db1f6c79b774cce73
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-16 00:46:32 +0200

    PLEASE GOD LAST

--- Body/Websites/Adopt/Adopt.php
+++ Body/Websites/Adopt/Adopt.php
@@ -74,8 +74,8 @@ include('../../Header/header.php'); ?>
                                 <div class=\"c4\"></div>
                                 <div class=\"b1\">
                                     <div class=\"b2\">
-                                        <select name='n'><option value='".$n."'></option></select>
-                                        <select name='a'><option value='".$a."'></option></select>
+                                        <select style='display: none' name='n'><option value='".$n."'></option></select>
+                                        <select style='display: none' name='a'><option value='".$a."'></option></select>
                                         <input style=\" font-size: 14pt\" type=\"submit\" value=\"Adopt Now\" name=\"adopt\" onclick=\"geekAlert()\"/>
                                     </div>
                                 </div>
