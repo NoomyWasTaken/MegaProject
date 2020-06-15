0eef8975c8ba8edd31696048c3877fb5d044c914
Neuman Alkhalil <neuman.alkhalil@outlook.com>
2020-06-16 01:16:18 +0200

    PLEASE GOD LAST

--- Body/Websites/Adopt/AdoptionForm/AdoptionForm.php
+++ Body/Websites/Adopt/AdoptionForm/AdoptionForm.php
@@ -1,12 +1,12 @@
 <?php
 if (isset($_POST['adopt'])) {
-
+    session_start();
     $conn = oci_connect('admin', 'Mimiplays23610', 'megaproject_high');
     $d = "18/06/2020";
     $n = $_POST['n'];
     $a = $_POST['a'];
 
-    $sql = 'INSERT INTO adoption_form (doa, users_id, animal_id) values (:doa, :uid, :aid)';
+    $sql = 'INSERT INTO adoption_form (doa, users_id, animal_id) VALUES (:doa, :uid, :aid)';
     $sql2 = 'UPDATE animal SET is_adopted = 1 WHERE animal_id = :aid2';
     $stid = oci_parse($conn, $sql);
     $stid2 = oci_parse($conn, $sql2);
